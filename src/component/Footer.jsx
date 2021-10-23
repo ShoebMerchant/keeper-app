e29182e3a01baf3da05a98cec7fba9839a32e303
Shoeb Merchant <shoebmerchant12@gmail.com>
2021-10-23 13:58:20 +0530

    Initial Commit

---
+++ src/component/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+  let year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ {year}</p>
+    </footer>
+  );
+}
