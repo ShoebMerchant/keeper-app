94d234cffad9c3ef90b35905b13c2e6a4c4dcd26
Shoeb Merchant <shoebmerchant12@gmail.com>
2021-10-23 14:13:49 +0530

    maping notes from notes.js

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -3,12 +3,17 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 
+import notes from "../notes";
+console.log(notes);
+
 export default function App() {
   return (
     <div>
       <Header />
       <Footer />
-      <Note />
+      {notes.map(note => (
+        <Note title={note.title} key={note.key} content={note.content} />
+      ))}
     </div>
   );
 }
