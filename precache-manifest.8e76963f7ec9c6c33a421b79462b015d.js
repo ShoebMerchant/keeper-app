3d12d05533d4faef69782fb7a38ec933e4fe6ae9
Shoeb Merchant <shoebmerchant12@gmail.com>
2021-11-11 17:17:02 +0530

    Updates

---
+++ precache-manifest.8e76963f7ec9c6c33a421b79462b015d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3416e21dac0c917c2aabcc9a931cd37f",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "2aee9c88ae7d5e3b6843",
+    "url": "/keeper-app/static/js/2.3392afc5.chunk.js"
+  },
+  {
+    "revision": "31620c1f29f314182e7a",
+    "url": "/keeper-app/static/js/main.7628e5bd.chunk.js"
+  },
+  {
+    "revision": "78797e502974e5d55e3c",
+    "url": "/keeper-app/static/js/runtime-main.e57cdbd2.js"
+  }
+]);
